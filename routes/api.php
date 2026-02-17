76a92b8c7513a07ce2518eb4974f628ded0c4025
Fauzi <mfauzi.itdev@gmail.com>
2026-02-17 11:58:19 +0700

    Update routes api.php

--- routes/api.php
+++ routes/api.php
@@ -6,26 +6,53 @@ use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\JobController;
 use App\Http\Controllers\Api\ApplicationController;
 
+/*
+|--------------------------------------------------------------------------
+| AUTH PUBLIC
+|--------------------------------------------------------------------------
+*/
+
 Route::prefix('auth')->group(function () {
     Route::post('/register/user', [AuthController::class, 'registerUser']);
     Route::post('/register/company', [AuthController::class, 'registerCompany']);
     Route::post('/login', [AuthController::class, 'login']);
 });
 
+/*
+|--------------------------------------------------------------------------
+| PUBLIC JOBS
+|--------------------------------------------------------------------------
+*/
+
 Route::prefix('jobs')->group(function () {
     Route::get('/', [JobController::class, 'getAll']);
     Route::get('/{id}', [JobController::class, 'publicShow']);
 });
 
+/*
+|--------------------------------------------------------------------------
+| AUTHENTICATED ROUTES
+|--------------------------------------------------------------------------
+*/
+
 Route::middleware('auth:sanctum')->group(function () {
 
+    /*
+    | AUTH USER
+    */
     Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/logout-all', [AuthController::class, 'logoutAll']);
         Route::get('/me', [AuthController::class, 'me']);
     });
 
-    Route::prefix('company')->group(function () {
+    /*
+    |--------------------------------------------------------------------------
+    | COMPANY AREA (ROLE: COMPANY)
+    |--------------------------------------------------------------------------
+    */
+    Route::middleware('role:company')->prefix('company')->group(function () {
+
         Route::get('/', [CompanyController::class, 'index']);
         Route::post('/', [CompanyController::class, 'store']);
         Route::put('/', [CompanyController::class, 'update']);
@@ -43,8 +70,14 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::get('/', [ApplicationController::class, 'getAllApplications']);
             Route::put('/{jobId}/{applicationId}', [ApplicationController::class, 'updateStatus']);
         });
+
     });
 
+    /*
+    |--------------------------------------------------------------------------
+    | JOB SEEKER APPLICATIONS
+    |--------------------------------------------------------------------------
+    */
     Route::prefix('applications')->group(function () {
         Route::post('/apply/{jobId}', [ApplicationController::class, 'store']);
         Route::get('/{jobId}/{applicationId}', [ApplicationController::class, 'detail']);
